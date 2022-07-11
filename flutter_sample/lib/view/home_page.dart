1cb3c2e0d9bcc0be409ac225c91a6c38c4483493
Souta Sugimoto <s-sugimoto@softnext.co.jp>
2022-07-11 14:54:37 +0900

    appbartとdrawerの変更と追加

--- flutter_sample/lib/view/home_page.dart
+++ flutter_sample/lib/view/home_page.dart
@@ -12,22 +12,204 @@ class HomePage extends ConsumerWidget {
     final homeNotifier = ref.watch(homeProvider.notifier);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter MVVM'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.only(top: 0),
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
             ),
-            Text(
-              '${homeState.count}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListTile(leading: const Icon(Icons.account_box), title: const Text("Google Sign In"), onTap: () {}),
+            ListTile(leading: const Icon(Icons.account_circle), title: const Text("Firebase Auth"), onTap: () {}),
+            ListTile(leading: const Icon(Icons.crop_original), title: const Text("Firestore,CloudVision"), onTap: () {}),
           ],
         ),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                );
+              },
+            ),
+            title: const Text('Welcome to Hokkaido'),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
+            flexibleSpace: Image.network(
+              'https://images.unsplash.com/photo-1514186077719-5f31a164c12e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2531&q=80',
+              fit: BoxFit.cover,
+            ),
+            toolbarHeight: 50,
+            collapsedHeight: 50,
+            expandedHeight: 200,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 400,
+                  width: 400,
+                  padding: const EdgeInsets.all(40),
+                  child: Card(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            'You have pushed the button this many times:',
+                          ),
+                          Text(
+                            '${homeState.count}',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: homeNotifier.increaseCount,
